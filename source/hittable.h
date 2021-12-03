1d571a3e124c2d05570c46a050dfd24f3fb0e913
wanting <liwanting12345@gmail.com>
2021-12-03 00:40:05 -0500

    image11

--- source/hittable.h
+++ source/hittable.h
@@ -1,13 +1,16 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
+#include "rtweekend.h"
+
+class material;
 
 struct hit_record {
     point3 p;
     vec3 normal;
     double t;
     bool front_face;
+    shared_ptr<material> mat_ptr;
 
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
